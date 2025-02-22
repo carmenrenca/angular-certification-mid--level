2898223998d053efb3a5f1c274709f7e46943f71
carmenrenca <carmenrenca3d@gmail.com>
2025-02-22 15:03:39 +0100

    PRIMER COMMIT

--- create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
+++ create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
-
-
+import { Component, input } from "@angular/core";
+import { Movie } from "../model/movie.model";
 @Component({
-  selector: 'app-movie-item',
+  selector: "app-movie-item",
   template: `
     <div class="movie-item">
       <div>
-        <h4>Test movie</h4>
+        <h4>movie().title</h4>
         <small class="subtitle">
-          <span>Release date: 2024-01-02</span>
+          <span>Release date: movie().release_date</span>
         </small>
       </div>
       <button>Details</button>
     </div>
   `,
   standalone: true,
-  styleUrls: [ 'movie-item.component.scss' ]
+  styleUrls: ["movie-item.component.scss"],
 })
 export class MovieItemComponent {
-
+  movie = input.required<Movie>();
 }
-
